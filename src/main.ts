73f78e6c8bd28359ab1b1fbe0a3668d44190e366
KANTNOLI <zahargysev02@gmail.com>
2025-03-23 23:24:15 +0300

    2

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,31 @@
+import { WebGLEngine } from "kgengine/Engine/VisualEngineConfigs/WebGLEngine.js"
 import { DefaultCameraSettings } from "kgengine/Engine/Cameras/DefaultCameraSettings.js";
 import { CreateScene } from "kgengine/Engine/OtherScripts/CreateScene.js";
-import { WebGLEngine } from "kgengine/Engine/VisualEngineConfigs/WebGLEngine.js";
+
+import { BoxGeometry } from "kgengine/Engine/Objects/Geometry/BoxGeometry.js";
+import { BasicMaterial } from "kgengine/Engine/Objects/Materials/BasicMaterial.js";
+
+import CustomObject from "kgengine/Engine/Objects/Snippets/CustomObject.js";
+
 
 const scene = new CreateScene();
-const camera = DefaultCameraSettings();
+const camera = DefaultCameraSettings({ x: 1, y: 1, z: 2 });
 
 const renderer = WebGLEngine();
+
+const cube = CustomObject(BoxGeometry(), BasicMaterial({ color: 0x00022 }));
+scene.addScene(cube);
+camera.lookAt(cube.position);
 
 document.body.appendChild(renderer.domElement);
 
 const animate = () => {
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+  cube.rotation.x += 0.01;
+
   renderer.render(scene.scene, camera);
   requestAnimationFrame(animate);
 };
 
-animate();s
+animate();
